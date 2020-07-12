8352a3f7e6383708b77679f0beaace61c2efd9d5
AshimaMehta <65941253+AshimaMehta@users.noreply.github.com>
2020-07-12 13:06:55 +0530

    File.py

---
+++ File .py
@@ -0,0 +1,19 @@
+from utilities import *
+
+args = get_args()
+traning_data = args['traning_data']
+testing_data = args['testing_data']
+
+training_features, traning_labels = get_data_details(traning_data)
+
+testing_features, testing_labels = get_data_details(testing_data)
+
+print("\n\n=-=-=-=-=-=-=- Decision Tree Classifier -=-=-=-=-=-=-=-\n")
+
+attack_classifier = tree.DecisionTreeClassifier()
+
+attack_classifier = attack_classifier.fit(training_features, traning_labels)
+
+predictions = attack_classifier.predict(testing_features)
+
+print("The precision of the Decision Tree Classifier is: " + str(get_occuracy(testing_labels,predictions, 1)) + "%")
